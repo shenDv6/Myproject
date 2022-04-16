eb12e61f4481e66f26d0488d48faa7b47b57c2e1
shenDv6 <sdustc@mail.ustc.edu.cn>
2022-04-16 21:29:17 +0800

    提交练习内容

--- project/src/App.js
+++ project/src/App.js
@@ -1,70 +1,52 @@
 import React, { Component } from 'react'
 
 
-function debounce(fn, wait) {
-  let timer = null;
-  let _this = this;
-  let count = 0;
-  return function() {
-    timer && clearTimeout(timer);
-    if (!count) {
-      fn.apply(_this, arguments);
-      timer = setTimeout(() => {
-        count = 0;
-      }, wait)
-    } else {
-      timer = setTimeout(() => {
-        fn.apply(_this, arguments);
-        count = 0;
-      }, wait)
-    }
-    count++;
-  }
-}
-
-let flag = true;
-function throttle(fn ,wait) {
-  return function () {
-    if (flag) {
-      setTimeout(() => {
-        fn.apply(this);
-        flag = true;
-      }, wait);
-    }
-    flag = false;
-  }
-}
+// function debounce(fn, wait) {
+//   let timer = null;
+//   let _this = this;
+//   let count = 0;
+//   return function () {
+//     timer && clearTimeout(timer);
+//     if (!count) {
+//       fn.apply(_this, arguments);
+//       timer = setTimeout(() => {
+//         count = 0;
+//       }, wait)
+//     } else {
+//       timer = setTimeout(() => {
+//         fn.apply(_this, arguments);
+//         count = 0;
+//       }, wait)
+//     }
+//     count++;
+//   }
+// }
 
 export default class App extends Component {
-  
+  state = {
+    titleValue: '',
+    buttonDisable:true
+  }
+
   handleChange = (event) => {
-    if ((event.type === 'keydown' && event.key === 'Enter') || event.type === 'click') {
-      document.title = this.nodeValue.value
+    const { value } = event.target;
+    if (value !== '') {
+      this.setState({buttonDisable: false})
+    } else {
+      this.setState({buttonDisable: true})
     }
+    this.setState({ titleValue: value });
   }
 
-  showMsg = () => {
-    console.log(1);
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', throttle(this.handleScroll, 500));
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll(){
-    console.log(document.documentElement.scrollTop);
+  changeTitle = () => {
+    document.title = this.state.titleValue;
   }
 
   render() {
     return (
-      <div style={{height:'2000px'}}>
-        <input ref={c=>this.nodeValue = c} onKeyDown={this.handleChange}/>
-        <button onClick={this.handleChange} style={{display:'block', width:'500px'}}>点我换标题</button>
-        <button onClick={debounce(this.showMsg, 500)}>防抖</button>
+      <div style={{ height: '2000px' }}>
+        <input onChange={this.handleChange} />
+        <button onClick={this.changeTitle} ref={c => this.titleButton = c} style={{ display: 'block', width: '500px' }} disabled = {this.state.buttonDisable}>点我换标题</button>
       </div>
     )
   }
